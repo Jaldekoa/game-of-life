e3a4480dde1bd263f78c05c07f97771fa5d280fe
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-27 23:15:31 +0100

    Fix some values

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 import { Board } from './src/Cell.js'
 
 const CELL_SIZE = 20;
-const CELL_COLOR = '#00FF00';
+const CELL_COLOR = '#e6e6e6';
 const BACKGROUND_COLOR = '#191919';
 const [HEIGHT, WIDTH] = [1280, 720];
-const [nX, nY] = [HEIGHT % CELL_SIZE, WIDTH % CELL_SIZE];
+const [nX, nY] = [HEIGHT / CELL_SIZE, WIDTH / CELL_SIZE];
 
 const $canvas = document.querySelector('canvas');
 const ctx = $canvas.getContext('2d');
@@ -16,4 +16,4 @@ $canvas.addEventListener('DOMContentLoaded', (event) => { });
 $run.addEventListener('click', (event) => { });
 $stop.addEventListener('click', (event) => { });
 
-const board = new Board(HEIGHT / CELL_SIZE, WIDTH / CELL_SIZE, CELL_COLOR, CELL_SIZE, ctx);
+const board = new Board(ctx, nX, nY, { alive: CELL_COLOR, dead: BACKGROUND_COLOR }, CELL_SIZE);
