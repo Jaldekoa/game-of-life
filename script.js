f0998a527aa1ebab4ecc6b95e9d44ed336ce7a85
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-28 00:59:38 +0100

    Add board, listeners and drawing functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { Board } from './src/Cell.js'
 
+let isDrawing = false;
 const CELL_SIZE = 20;
 const CELL_COLOR = '#e6e6e6';
 const BACKGROUND_COLOR = '#191919';
@@ -12,8 +13,41 @@ const ctx = $canvas.getContext('2d');
 const $run = document.getElementById('run');
 const $stop = document.getElementById('stop');
 
+const board = new Board(ctx, nX, nY, { alive: CELL_COLOR, dead: BACKGROUND_COLOR }, CELL_SIZE);
+
+// Canvas management
 $canvas.addEventListener('DOMContentLoaded', (event) => { });
-$run.addEventListener('click', (event) => { });
+$canvas.addEventListener('mousedown', startDrawing);
+$canvas.addEventListener('mousemove', draw);
+$canvas.addEventListener('mouseup', stopDrawing);
+$canvas.addEventListener('mouseleave', stopDrawing);
+
+// Game manegement
+$run.addEventListener('click', (event) => {
+    event.preventDefault();
+    $run.setAttribute('disabled', '');
+    board.start();
+});
+
 $stop.addEventListener('click', (event) => { });
 
-const board = new Board(ctx, nX, nY, { alive: CELL_COLOR, dead: BACKGROUND_COLOR }, CELL_SIZE);
+function startDrawing() { isDrawing = true };
+function stopDrawing() { isDrawing = false };
+function draw(event) {
+    if (!isDrawing) return;
+
+    const { offsetX, offsetY } = event;
+
+    let x = Math.floor(offsetX / CELL_SIZE);
+    let y = Math.floor(offsetY / CELL_SIZE);
+
+    // Limitar a rango válido
+    x = Math.max(0, Math.min(x, board.nx - 1));
+    y = Math.max(0, Math.min(y, board.ny - 1));
+
+    console.log(x, y);
+
+    // Alternar estado
+    board.board[y][x].isAlive = !board.board[y][x].isAlive;
+}
+
